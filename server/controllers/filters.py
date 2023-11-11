edc7ddc61d2586e8255a0d005370554d06776081
Adrian Araya <44324449+Heutlett@users.noreply.github.com>
2023-11-10 22:32:19 -0600

    se actualizan el controlador de los filtros con los comandos de libmygpio

--- server/controllers/filters.py
+++ server/controllers/filters.py
@@ -1,24 +1,15 @@
 from libs.rpigpio import *
 from config import constants
 
-
-def finish() -> None:
-    """
-    This function clears the pins used by filters.
-    """
-    for pin in constants.PINS['filters'].values():
-        unexport_command(pin)
-
 def start() -> None:
     """
     This function starts the pins used by filters.
     """
+    gpio_rpi_init_ptrs()
+    
     for filter, pin in constants.PINS['filters'].items():
-        if export_command(pin) == constants.FAIL:
-            print(f'Filter Error: Pin {pin} for {filter} failed to start.')
-        else:
-            if pin_mode(pin, constants.INPUT_MODE) == constants.FAIL:
-                print(f'Filter Error: Pin {pin} for {filter} failed to start.')
+        gpio_rpi_set_mode(pin=pin, mode=constants.INPUT_MODE)
+        print(f'Pin {pin} set as input for filter {filter}.')
 
 def get_state(filter: str) -> int:
     """
@@ -36,7 +27,7 @@ def get_state(filter: str) -> int:
     pin: int = constants.PINS['filters'][filter]
 
     # Get the state of the door
-    result = digital_read(pin)
+    result = gpio_rpi_read(pin=pin)
 
     if (result == constants.FAIL):
         print('Filter Error: {filter} in pin {pin} is not available.')
